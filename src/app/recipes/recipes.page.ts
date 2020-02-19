bb3607bf8cd772488e123993d3734c6f6629cdd1
gregchaves <gchavesacc@gmail.com>
2020-02-19 16:11:05 -0300

    bug fixing

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Recipe } from './recipe.model';
 import { RecipesService } from './recipes.service';
 
@@ -7,14 +7,37 @@ import { RecipesService } from './recipes.service';
   templateUrl: './recipes.page.html',
   styleUrls: ['./recipes.page.scss'],
 })
-export class RecipesPage implements OnInit {
-  recipes: Recipe[];
+export class RecipesPage implements OnInit, OnDestroy {
 
   constructor(private recipesService: RecipesService) { }
 
-  ngOnInit() {
+  recipes: Recipe[];
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy');
+  }
+
+  ionViewWillEnter() {
+    console.log('ionViewWillEnter');
     this.recipes = this.recipesService.getAllRecipes();
     console.log(this.recipes);
+    console.log('Fucking recipes');
+  }
+
+  ionViewDidEnter() {
+    console.log('ionViewDidEnter');
+  }
+
+  ionViewWillLeave() {
+    console.log('ionViewWillLeave');
+  }
+
+  ionViewDidLeave() {
+    console.log('ionViewDidLeave');
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit');
   }
 
 }
